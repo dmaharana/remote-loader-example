9670de707cff8d9eeaf60f12490beae7b11a6dd5
Deepak Maharana <dmaharana@gmail.com>
2021-11-12 13:48:38 +0530

    added clone statement

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,6 @@
 node('master') {
+    cleanWs()
+    sh 'git clone https://github.com/dmaharana/remote-loader-example.git .'
     // Load file from the current directory:
     def externalMethod = load("externalMethod.groovy")
     externalMethod.lookAtThis("Steve")
